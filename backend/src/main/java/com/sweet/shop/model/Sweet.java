4ec92410fe6c48790ecc60821cf6f0bf05430305
Nikita <imnikitajaiswal@gmail.com>
2025-12-16 01:58:10 +0530

    Added backend Spring Boot project into /backend folder

---
+++ backend/src/main/java/com/sweet/shop/model/Sweet.java
@@ -0,0 +1,60 @@
+package com.sweet.shop.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Sweet {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private String category;
+    private double price;
+    private int quantity;
+
+    // getter
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    // setter
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
